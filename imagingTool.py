295b213a56e3deb2fee53b867028afac6fca5d4f
TevinG777 <gajadhartevin@gmail.com>
2024-03-27 13:25:29 -0400

    colorMask working

--- imagingTool.py
+++ imagingTool.py
@@ -21,23 +21,17 @@ def dcmToImage(ds):
     return image
 
 def colorRangeGraph(image):
-    # add all of the y values together
-    y = np.sum(image, axis=0)
-    
-    # add total amount of x value entries
-    x = len(y)
-
-    # sum all y values
-    ySum = np.sum(y)
-
-    mean = ySum / x
+    # find the avevage xvalue of the image
+    meanIndex = np.mean(image.ravel())
 
     # display graph of the image color range with mean
     
     plt.hist(image.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
-    plt.axvline(mean, color='r', linestyle='dashed', linewidth=2)
+    plt.axvline(meanIndex, color='r', linestyle='dashed', linewidth=2)
     
     plt.show()
+
+    return meanIndex
 
 
 
@@ -52,7 +46,22 @@ def main():
     image = dcmToImage(ds)
     #cv2.imshow('image', image)
     #cv2.waitKey(0)
-    colorRangeGraph(image)
+    thresh = colorRangeGraph(image)
+
+    print(thresh)
+
+    # use cv2 to create a color mask to only select pixels that are above the threshold
+    mask = cv2.inRange(image, thresh, 1.0)
+
+    # apply the mask to the image
+    #result = cv2.bitwise_and(image, image, mask=mask)
+
+    
+
+    # display the result
+    cv2.imshow('result', mask)
+    cv2.waitKey(0)
+
 
 main()
 
