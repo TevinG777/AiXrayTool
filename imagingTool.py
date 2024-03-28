ec0a213a30c721a6798eeaef995ad6513b9ec67b
TevinG777 <gajadhartevin@gmail.com>
2024-03-28 12:35:26 -0400

    big progress te location

--- imagingTool.py
+++ imagingTool.py
@@ -24,43 +24,95 @@ def colorRangeGraph(image):
     # find the avevage xvalue of the image
     meanIndex = np.mean(image.ravel())
 
+    # move the mean slightly to the left using standard deviation
+    meanIndex = meanIndex - 0.4*np.std(image.ravel())
     # display graph of the image color range with mean
     
-    plt.hist(image.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
-    plt.axvline(meanIndex, color='r', linestyle='dashed', linewidth=2)
-    
-    plt.show()
+    #plt.hist(image.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
+    #plt.axvline(meanIndex, color='r', linestyle='dashed', linewidth=2)
+    #plt.show()
 
     return meanIndex
 
+def TEfinder(image):
+     # Convert the image to grayscale
+    if len(image.shape) > 2:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # Convert the image to 8-bit
+    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    
+    # apply thresholding to the image
+    _, image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # find the contours in the image
+    countours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # find ovals in the image
+    oval_contours = []
+    for cnt in countours:
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)  
+        if len(approx) >= 5:  
+            area = cv2.contourArea(cnt)  
+            if area <200:  
+                oval_contours.append(cnt)
+                
+
+    # Convert image to color
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    xList = [0]
+    yList = [0]
+
+    # grab x, y values of the oval contours
+    for cnt in oval_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        xList.append(x)
+        yList.append(y)
+    
+    # plot all of the x and y values on a histogram
+    print(xList)
+
+    # find max x and y values
+    #x = max([x for x, _, _, _ in [cv2.boundingRect(cnt) for cnt in oval_contours]])
+    #y = max([y for _, y, _, _ in [cv2.boundingRect(cnt) for cnt in oval_contours]])
+
+
+
+
+
+    # display the image
+
+    return image   
 
 
 def main():
+    # Read the dicom file
     inputDir = 'dcmHoldingFolder'
-
     file = os.path.join(inputDir, 'xray.dcm')
 
+    # Convert the dicom file to an image
     ds = pydicom.dcmread(file)
-
     image = dcmToImage(ds)
-    #cv2.imshow('image', image)
-    #cv2.waitKey(0)
-    thresh = colorRangeGraph(image)
 
-    print(thresh)
 
-    # use cv2 to create a color mask to only select pixels that are above the threshold
-    mask = cv2.inRange(image, thresh, 1.0)
+    # Iterate the mask to refine the image
+    for i in range(3):
+        thresh = colorRangeGraph(image)
+        mask = cv2.inRange(image, thresh, 1.0)
+        image = cv2.bitwise_and(image, image, mask=mask)
 
-    # apply the mask to the image
-    #result = cv2.bitwise_and(image, image, mask=mask)
+    # Pass image into TE finder
+    imageTE = TEfinder(image)
+        
+    # Display the image with the TE finder and the original image
+    plt.imshow(imageTE)
+    plt.show()
 
+   
     
 
-    # display the result
-    cv2.imshow('result', mask)
-    cv2.waitKey(0)
 
 
 main()
